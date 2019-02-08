9fb22c0e662c5cb150dd94f34fe5301c6c20ad25
Gino Wu <gino.wu7@gmail.com>
2019-02-08 11:13:15 -0500

    Updated dates on AB tests

--- ABTestMaintenanceStrategy/VariantProviding.swift
+++ ABTestMaintenanceStrategy/VariantProviding.swift
@@ -11,16 +11,16 @@ import Foundation
 public protocol DynamicVariableProviding: class {
     static func load()
 
-    var enableCloseAccountOption: Bool { get } // AB_TEST: Gino Wu | Reg squad | Exp 02/02/2019
-    var iraVideoValuePropYoutubeId: String { get } // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
-    var custodianVideoValuePropYoutubeId: String { get }
-    var investVideoValuePropYoutubeId: String { get }
-    var securitiesLendingVariables: String { get } // AB_TEST: Gino Wu | Reg squad | Exp 10/10/2018
-    var prefundVideoUrl: URL? { get }
-    var postPurchaseValuePropType: String { get }
-    var postPurchaseValuePropDeepLink: String { get }
-    var foundersCardVideoUrl: URL? { get }
-    var autoStashScheduleType: String { get }
+    var enableCloseAccountOption: Bool { get }              // AB_TEST: Gino Wu | Reg squad | Exp 02/02/2019
+    var iraVideoValuePropYoutubeId: String { get }          // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
+    var custodianVideoValuePropYoutubeId: String { get }    // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
+    var investVideoValuePropYoutubeId: String { get }       // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
+    var securitiesLendingVariables: String { get }          // AB_TEST: Gino Wu | Reg squad | Exp 10/10/2018
+    var prefundVideoUrl: URL? { get }                       // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
+    var postPurchaseValuePropType: String { get }           // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
+    var postPurchaseValuePropDeepLink: String { get }       // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
+    var foundersCardVideoUrl: URL? { get }                  // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
+    var autoStashScheduleType: String { get }               // AB_TEST: Gino Wu | Growth squad | Exp 01/02/2030
 }
 
 
